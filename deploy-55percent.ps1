53af77e6a9e6868eccaa491395294765d72c95e0
Mohd Ali <mohd.ali.el@gmail.com>
2026-09-08 23:37:26 -0500

    Deploy site from GitHub on VPS

--- deploy-55percent.ps1
+++ deploy-55percent.ps1
@@ -1,15 +1,11 @@
 $ErrorActionPreference = 'Stop'
 $server = 'root@137.184.210.97'
-$domain = '55percent.org'
 $remoteRoot = '/var/www/55percent'
-$remoteCert = '/root/55percent.org-cert'
 
-Write-Host 'Uploading site files...' -ForegroundColor Cyan
-ssh $server "mkdir -p $remoteRoot $remoteCert"
-scp ./index.html ./styles.css ./app.js "$server`:$remoteRoot/"
+Write-Host 'Pushing current branch to GitHub...' -ForegroundColor Cyan
+git push origin main
 
-Write-Host 'Uploading nginx configuration...' -ForegroundColor Cyan
-scp ./55percent.nginx "$server`:/etc/nginx/sites-available/55percent.org"
-ssh $server "ln -sf /etc/nginx/sites-available/55percent.org /etc/nginx/sites-enabled/55percent.org && nginx -t && systemctl reload nginx"
+Write-Host 'Pulling the latest commit on the VPS...' -ForegroundColor Cyan
+ssh $server "git -C $remoteRoot fetch origin main && git -C $remoteRoot reset --hard origin/main && chown -R www-data:www-data $remoteRoot && nginx -t && systemctl reload nginx && git -C $remoteRoot log -1 --oneline"
 
-Write-Host 'Site deployed. HTTPS will be enabled after the certificate is provisioned.' -ForegroundColor Green
+Write-Host 'Git deployment complete.' -ForegroundColor Green
